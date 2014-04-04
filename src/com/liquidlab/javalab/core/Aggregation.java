6e5004fb81200e7e2ddae399820879c93560b26b
Thamizh Arasu <zarub2k@gmail.com>
2014-04-04 22:44:28 +0530

    Aggregation is added

---
+++ src/com/liquidlab/javalab/core/Aggregation.java
@@ -0,0 +1,20 @@
+package com.liquidlab.javalab.core;
+
+import com.liquidlab.javalab.common.Initializer;
+import com.liquidlab.javalab.common.Person;
+
+import java.util.List;
+
+/**
+ * @author tham
+ */
+public class Aggregation {
+    public int getAverageAge() {
+        List<Person> persons = Initializer.getPersons();
+//        int highestAge =
+//                persons.stream()
+//                        .mapToInt(p -> p.getAge())
+//                        .max();
+        return 0;
+    }
+}
